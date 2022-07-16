707ec28d09a58369443250141709a0b8266bf632
Daniel M <danielm-github@dnml.de>
2022-07-16 19:23:28 +0200

    Add fromArray benchmark and pull latest optimizations

---
+++ benchmark/bench/fromArray.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var Benchmark = require('benchmark');
+var suite = new Benchmark.Suite();
+
+var Denque = require('../../denque');
+var DenqueMod = require('../../denque-dnlmlr');
+var DoubleEndedQueue = require('double-ended-queue');
+
+var l = 100000;
+
+var baseArray = new Array(l);
+
+while (--l) {
+  baseArray.push(l);
+}
+
+suite
+  .add('denque', function () {
+    var queue = new Denque(baseArray);
+  })
+  .add('denque (mod)', function () {
+    var queue = new DenqueMod(baseArray);
+  })
+  .add('double-ended-queue', function () {
+    var queue = new DoubleEndedQueue(baseArray);
+  })
+  .on('cycle', function (e) {
+    console.log('' + e.target);
+  })
+  .run();
